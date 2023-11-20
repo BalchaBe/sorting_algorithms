8b28a218ef62a5c6a8b6332fa3caa0913d10ff76
BalchaBe <balcha36@yahoo.com>
2023-11-20 03:27:04 -0800

    error correction

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -1,93 +1,70 @@
 #include "sort.h"
-#include <stdio.h>
 /**
- * swap - change two values in ascending or descending order
- * @arr: array
- * @item1: item one
- * @item2: item two
- * @order: 1: ascending order, 0 descending order
- */
-void swap(int arr[], int item1, int item2, int order)
+* partition - quicksort using Hoare version
+* @array: array to sort
+* @min: lowest index
+* @max: highest index
+* @size: size of the array
+*
+* Return: partition index
+*/
+size_t partition(int *array, ssize_t min, ssize_t max, size_t size)
 {
-	int temp;
+	int swap, pivot;
 
-	if (order == (arr[item1] > arr[item2]))
+	pivot = array[max];
+	while (min <= max)
 	{
-		temp = arr[item1];
-		arr[item1] = arr[item2];
-		arr[item2] = temp;
-	}
-}
-/**
- * merge - sort bitonic sequences recursively in both orders
- * @arr: array
- * @low: first element
- * @nelemnt: elements number
- * @order: 1: ascending order, 0 descending order
- */
-void merge(int arr[], int low, int nelemnt, int order)
-{
-	int mid, i;
-
-	if (nelemnt > 1)
-	{
-		mid = nelemnt / 2;
-		for (i = low; i < low + mid; i++)
-			swap(arr, i, i + mid, order);
-		merge(arr, low, mid, order);
-		merge(arr, low + mid, mid, order);
-	}
-}
-/**
- * bitonicsort - bitonic sort algorithm implementation
- * @arr: array
- * @low: first element
- * @nelemnt: number of elements
- * @order: 1: ascending order, 0 descending order
- * @size: array lenght
- */
-void bitonicsort(int arr[], int low, int nelemnt, int order, int size)
-{
-	int mid;
-
-	if (nelemnt > 1)
-	{
-		if (order >= 1)
+		while (array[min] < pivot)
+			min++;
+		while (array[max] > pivot)
+			max--;
+		if (min <= max)
 		{
-			printf("Merging [%i/%i] (UP):\n", nelemnt, size);
-			print_array(&arr[low], nelemnt);
-		}
-		else
-		{
-			printf("Merging [%i/%i] (DOWN):\n", nelemnt, size);
-			print_array(&arr[low], nelemnt);
-		}
-		mid = nelemnt / 2;
-		bitonicsort(arr, low, mid, 1, size);
-		bitonicsort(arr, low + mid, mid, 0, size);
-		merge(arr, low, nelemnt, order);
-		if (order <= 0)
-		{
-			printf("Result [%i/%i] (DOWN):\n", nelemnt, size);
-			print_array(&arr[low], nelemnt);
-		}
-		if (order >= 1)
-		{
-			printf("Result [%i/%i] (UP):\n", nelemnt, size);
-			print_array(&arr[low], nelemnt);
+			if (min != max)
+			{
+				swap = array[min];
+				array[min] = array[max];
+				array[max] = swap;
+				print_array(array, size);
+			}
+			min++;
+			max--;
 		}
 	}
+	return (max);
 }
-/**
- * bitonic_sort - prepare the terrain to bitonic sort algorithm
- * @array: array
- * @size: array lenght
- */
-void bitonic_sort(int *array, size_t size)
-{
-	int order = 1;
 
-	if (!array || size < 2)
+/**
+* quicksort - sorts a partition of an array of integers
+* @array: array to sort
+* @min: lowest index of the partition to sort
+* @max: highest index of the partition to sort
+* @size: size of the array
+*
+* Return: void
+*/
+void quicksort(int *array, ssize_t min, ssize_t max, size_t size)
+{
+	ssize_t pivot;
+
+	if (min < max)
+	{
+		pivot = partition(array, min, max, size);
+		quicksort(array, min, pivot, size);
+		quicksort(array, pivot + 1, max, size);
+
+	}
+}
+
+/**
+* quick_sort_hoare - sorts an array of integer with quick sort
+* @array: The array to sort
+* @size: The size of the array
+*/
+void quick_sort_hoare(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
 		return;
-	bitonicsort(array, 0, size, order, size);
+	quicksort(array, 0, size - 1, size);
 }
