6cad4cf4925957bfa9fed25b6186eb73899f6d94
BalchaBe <balcha36@yahoo.com>
2023-11-20 03:02:25 -0800

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,37 @@
 #include "sort.h"
+void swap(int *a, int *b);
 /**
- * bubble_sort - sort array lements from min to max value
- * @array: array
- * @size: array size
+ * bubble_sort - sort an array of integer in ascending order
+ * @array: the array to order and print
+ * @size: size of array to order
  */
 void bubble_sort(int *array, size_t size)
 {
+	unsigned int m = 0, n = 0;
 
-	size_t m, index, t = 0;
-
-	if (size < 2)
+	if (size <= 1)
 		return;
-	for (m = 0; m < size; m++)
-		for (index = 0; index < size; index++)
-		{
-			if (array[index] > array[index + 1] && array[index + 1])
-			{
-			t = array[index];
-			array[index] = array[index + 1];
-			array[index + 1] = t;
-			print_array(array, size);
-			}
-		}
 
+	for (m = 0; m < (size - 1); m++)
+		for (n = 1; n < size; n++)
+			if (array[n - 1] > array[n])
+			{
+				swap(&array[n - 1], &array[n]);
+				print_array(array, size);
+			}
+}
+
+/**
+ * swap - swap two pointers to integer
+ *
+ * @a: first int pointer
+ * @b: second int pointer
+ */
+void swap(int *a, int *b)
+{
+	int swap;
+
+	swap = *a;
+	*a = *b;
+	*b = swap;
+}
